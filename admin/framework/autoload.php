de5ed6ee04fc38d005d093bbe9b48324050788f0
AlexAlexandru <alexandru_mandric@yahoo.com>
2016-08-12 12:44:22 +0300

    [Fix]Fatal error

--- admin/framework/autoload.php
+++ admin/framework/autoload.php
@@ -4,12 +4,16 @@ if (!function_exists('array_column')) {
     require_once(plugin_dir_path(__FILE__).'/vendor/array_column.php');
 }
 
-function __autoload($name){
+if(!function_exists('sse_autoloader')){
 	
-    $class_name = $name.'.class.php';
-	$path = plugin_dir_path( __FILE__ ).$class_name;
-	
-    if(file_exists($path)){
-		require_once($class_name);
+	function sse_autoloader($name){
+		
+		$class_name = $name.'.class.php';
+		$path = plugin_dir_path( __FILE__ ).$class_name;
+		
+		if(file_exists($path)){
+			require_once($class_name);
+		}
 	}
 }
+spl_autoload_register('sse_autoloader');
